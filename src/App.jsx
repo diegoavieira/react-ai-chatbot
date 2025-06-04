729d58d57d7cc5013aea99c8134db4fe4b61226d
Diego <diegoavieira@gmail.com>
2025-06-04 11:57:39 -0300

    Aplicado estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import styles from './App.module.css';
+import { Chat } from './components/Chat/Chat';
+import { Controls } from './components/Controls/Controls';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState([]);
+
+  function handleContentSend(content) {
+    setMessages((prevMessages) => [...prevMessages, { content, role: 'user' }]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" alt="Chatbot Logo" />
+        <h2 className={styles.Title}>React AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      <Controls onSend={handleContentSend} />
+    </div>
   );
 }
 
